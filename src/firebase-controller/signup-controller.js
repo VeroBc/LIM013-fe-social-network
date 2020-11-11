37d4e90c02663570200b1477325c52d4949de2e3
Vero Bc <verobc.cel@gmail.com>
2020-11-11 11:35:39 -0500

    temporary changes for demo

--- src/firebase-controller/signup-controller.js
+++ src/firebase-controller/signup-controller.js
@@ -1,14 +1,24 @@
-import { createUserAccount } from '../firebase/authentication.js';
+// import { createUserAccount } from '../firebase/authentication.js';
 
-export const createUser = (email, password) => {
-  createUserAccount(email, password)
-    .then(() => {
-      window.location.hash = '#/home';
-    })
-    .catch((e) => {
-      const messageError = document.createElement('p');
-      messageError.innerHTML = e.message;
-      document.getElementById('container').appendChild(messageError);
-      return messageError;
-    });
+// export const createUser = (email, password) => {
+//   createUserAccount(email, password)
+//     .then(() => {
+//       window.location.hash = '#/home';
+//     })
+//     .catch((e) => {
+//       const messageError = document.createElement('p');
+//       messageError.innerHTML = e.message;
+//       document.getElementById('container').appendChild(messageError);
+//       return messageError;
+//     });
+// };
+
+import { createUserAccount } from '../firebase/autentication.js';
+
+export const userSignUp = () => {
+  // const username = document.querySelector('#username');
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
+
+  createUserAccount(email, password);
 };
