d95209615f075ba3a812fa57dcd1f6926f3d5a4a
Vero Bc <verobc.cel@gmail.com>
2020-11-16 11:48:09 -0500

    logout button and current user console.log info

--- view/home.js
+++ view/home.js
@@ -1,9 +1,20 @@
+import { logginOut, loadInfoUser } from '../view-controller/home-controller.js';
+
 export default () => {
+  loadInfoUser();
   const viewHome = `
-  <h1>Q&A LABORATORIA</h1>`;
+  <h1>Q&A LABORATORIA</h1>
+  <button id="signOutButton">Cerrar sesión</button>
+  `;
   document.getElementById('container').classList.remove('main');
   const sectionElement = document.createElement('section');
   sectionElement.classList.add('position');
   sectionElement.innerHTML = viewHome;
+
+  const signOutButton = sectionElement.querySelector('#signOutButton');
+  signOutButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    logginOut();
+  });
   return sectionElement;
 };
