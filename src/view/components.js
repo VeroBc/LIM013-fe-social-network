df3051df5a73f62db21f7001022331ad8d347c9f
Vero Bc <verobc.cel@gmail.com>
2020-11-19 11:06:03 -0500

    home_view component

--- src/view/components.js
+++ src/view/components.js
@@ -1,9 +1,11 @@
 import signUp from './signup.js';
 import signIn from './signin.js';
 import Home from './home.js';
+import Profile from './profile.js';
 
 export const components = {
   signup: signUp,
   signin: signIn,
   home: Home,
+  profile: Profile,
 };
