3d9001328b0b7b642b5394199b7c79861e8c3a6f
Sharon Huaman <sharonb.huaman@gmail.com>
2020-11-24 13:52:49 -0500

    changes in signup-controller

--- firebase-controller/signup-controller.js
+++ firebase-controller/signup-controller.js
@@ -5,18 +5,17 @@ const imgDefault = 'gs://qa-lab-c5336.appspot.com/user-default.svg';
 export const createUser = (email, password) => {
   const messageError = document.getElementById('errorMessage');
   createUserAccount(email, password)
+    .then(() => user())
     .then((currentUser) => {
       inToUser({
         uid: currentUser.uid,
         mail: currentUser.email,
         name: /^([^]+)@(\w+).(\w+)$/.exec(currentUser.email)[1],
         photo: imgDefault,
-        pass: password,
       });
     })
     .then(() => {
       window.location.hash = '#/home';
-      return user();
     })
     .catch((e) => {
       messageError.innerHTML = e.message;
