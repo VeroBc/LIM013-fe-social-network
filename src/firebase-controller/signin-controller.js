c351c59f7b5b50c9782d77c0e6546a9de9754586
Vero Bc <verobc.cel@gmail.com>
2020-11-25 07:20:35 -0500

    loading user info from collection

--- src/firebase-controller/signin-controller.js
+++ src/firebase-controller/signin-controller.js
@@ -1,8 +1,20 @@
-import { signinUserAccount } from '../firebase/autentication.js';
+import { signinUserAccount, user } from '../firebase/autentication.js';
+import { inToUser, getUser } from '../firebase/store.js';
 
+const imgDefault = 'gs://qa-lab-c5336.appspot.com/user-default.svg';
 export const signinUser = (email, password) => {
   const errorMessage = document.getElementById('errorMessages');
   signinUserAccount(email, password)
+    .then(() => user())
+    .then((currentUser) => {
+      inToUser({
+        uid: currentUser.uid,
+        mail: currentUser.email,
+        name: /^([^]+)@(\w+).(\w+)$/.exec(currentUser.email)[1],
+        photo: imgDefault,
+      });
+    })
+    .then(() => getUser())
     .then(() => {
       window.location.hash = '#/home';
     })
--- src/view/home.js
+++ src/view/home.js
@@ -1,43 +1,44 @@
 import { signOutUser } from '../firebase-controller/signout-controller.js';
+import { getUser } from '../firebase/store.js';
 
 export default () => {
-  const userPicture = localStorage.getItem('userpicture');
-  const userName = localStorage.getItem('username');
-  const userEmail = localStorage.getItem('useremail');
+  const user = firebase.auth().currentUser;
+  if (!user) {
+    window.location.hash = '#/signin';
+  }
+  getUser();
   const viewHome = `
     <header class="mainHead">
-        <img id="profileView" class="userPicture" src="${userPicture || './img/user-default.svg'}">
+        <img id="profileView" class="userPicture" src="${user.photoURL || './img/user-default.svg'}">
         <img src="./img/logo-lab-white.svg" alt="Q&A" class="logo">
         <i class="fas fa-sign-out-alt" id="signOutButton"></i>
     </header>
     <aside class="side">
-        <img class="userPictureAside" src="${userPicture || './img/user-default.svg'}">
-        <p class="userName">${userName || 'Username'}</p>
-        <p class="userEmail">${userEmail || 'email@mail.com'}</p>
+        <img class="userPictureAside" src="${user.photoURL || './img/user-default.svg'}">
+        <p class="userName" id="userName"></p>
+        <p class="userEmail" id="userEmail"></p>
     </aside>
     <article class="content">
         <textarea class="inputPosts" placeholder="Escribe tu pregunta aquí" ></textarea>
         <button type="submit" id="postsButton">Publicar</button>
         <section class="publicSide">
-            <img class="publicPicture" src="${userPicture || './img/user-default.svg'}">
-            <p class="publicName">${userName || 'Username'}</p>
+            <img class="publicPicture" src="${user.photoURL || './img/user-default.svg'}">
+            <p class="publicName">${user.email.match(/^([^@]*)@/)[1] || 'Username'}</p>
             <textarea class="publicPosts">Hola chicas! Alguien podrá ayudarme con los arrays?</textarea>
         </section>
         <section class="publicSide">
-            <img class="publicPicture" src="${userPicture || './img/user-default.svg'}">
-            <p class="publicName">${userName || 'Username'}</p>
+            <img class="publicPicture" src="${user.photoURL || './img/user-default.svg'}">
+            <p class="publicName">Username</p>
             <textarea class="publicPosts">Hola! Alguien sabe como utilizar CSS grid?</textarea>
         </section>
         <section class="publicSide">
-            <img class="publicPicture" src="${userPicture || './img/user-default.svg'}">
-            <p class="publicName">${userName || 'Username'}</p>
+            <img class="publicPicture" src="${user.photoURL || './img/user-default.svg'}">
+            <p class="publicName">Username</p>
             <textarea class="publicPosts">Hola a todos! Alguien tendrá algun recurso de Git?</textarea>
         </section>
     </article> 
     
     `;
-  //   document.getElementById('container').classList.remove('main');
-  //   document.getElementById('container').classList.add('main');
   const sectionElement = document.createElement('div');
   sectionElement.classList.add('homeContainer');
   sectionElement.innerHTML = viewHome;
